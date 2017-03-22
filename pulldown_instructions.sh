89b47414c8411a7bfbba152708b3194a1c390f3f
Catharine McGhan <cmcghan@users.noreply.github.com>
2017-03-21 20:44:48 -0400

    Create pulldown_instructions.sh

---
+++ pulldown_instructions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2017 University of Cincinnati
+# All rights reserved. See LICENSE file at:
+# https://github.com/AS4SR/website
+# Additional copyright may be held by others, as reflected in the commit history.
+cd /home/spacerobotics
+mkdir -p git_pulls
+cd git_pulls
+rm master
+rm -rf website-master
+wget https://github.com/AS4SR/website/archive/master.zip
+unzip master
+cd /home/spacerobotics
+#cp -R public_html public_html_old
+#cd public_html
+#rm -rf /home/spacerobotics/public_html/*
+rm -rf public_html
+cp -R /home/spacerobotics/git_pulls/website-master/public_html public_html
